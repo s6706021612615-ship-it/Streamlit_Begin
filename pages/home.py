00c0cf909c844a3a83e8113af1a35abd93207d3c
/ <s6706021612615@email.kmutnb.ac.th>
2026-09-21 16:46:43 +0700

    Initial commit

---
+++ pages/home.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as st
+
+
+# Title
+st.title("Web Application for Model Deployment")
+st.header("Data Science Group")
+st.subheader(":blue[Applied Machine Learning Class 1/2569] :sunglasses:")
+st.subheader(":red[Created by ] :orange[Jirameth] :green[Lertsakwiman] :blue[..]:blossom:")
